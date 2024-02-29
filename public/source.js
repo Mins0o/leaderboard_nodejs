b71cf6aff74b691dcc0f4a3c92caa86a25dd9a09
Minsoo Kang <memesoo.kang@gmail.com>
2024-03-01 02:56:27 +0900

    Encapsulating Elo + restructuring callback setting
    I am still learning on how the eventlisteners work.
    
    Added feature: submit minimally valid input to clear the input.

--- public/source.js
+++ public/source.js
@@ -7,6 +7,34 @@ class Elo {
                     [0,1,1],
                     [0,0,1],
                     [0,0,0]];
+
+  get_win_probability(rating_a, rating_b){
+    let q_a = exp10(rating_a/400);
+    let q_b = exp10(rating_b/400);
+    let expected_a = q_a/(q_a+ q_b);
+    let expected_b = 1 - expected_a;
+    return ([expected_a, expected_b]);
+  }
+
+  get_win_probabilities(elo_array){
+    let number_of_players = elo_array.length;
+    let win_prob_array = manual_copy(g_win_prob_template);
+    for(let ii=0; ii<number_of_players; ii++){
+        for(let jj=ii-1; jj>=0; jj--){
+            let win_prob = get_win_probability(elo_array[jj],elo_array[ii]);
+            win_prob_array[jj][ii-1] = win_prob[0];
+            win_prob_array[ii][jj] = win_prob[1];
+        }
+    }
+    return win_prob_array;
+  }
+
+  get_elo(player){
+    if(g_elo_lookup[player] == null ){ // may be null, undefined
+        g_elo_lookup[player] = 1500; // create the new player
+    }
+    return g_elo_lookup[player];
+  }
 }
 
 class ServerComm{
@@ -58,28 +86,40 @@ class ServerComm{
 class ElementsController{
   matchRecordTable = document.getElementsByClassName("match-record-table")[0];
   suggestionForm = document.getElementsByClassName("suggestion-form")[0];
+  #previousCallBack = (a,b,c,d)=>{};
+  #basicSubmitAction = (callback) => (event) => {
+    // Prevent the default behavior of the form submission
+    event.preventDefault();
+
+    // Get the input element
+    let date = document.getElementsByClassName('date-input')[0];
+    let p1 = document.getElementsByClassName('p1-input')[0];
+    let p2 = document.getElementsByClassName('p2-input')[0];
+    let p3 = document.getElementsByClassName('p3-input')[0];
+    let p4 = document.getElementsByClassName('p4-input')[0];
+
+    // Check if the input value is not empty
+    if (date.value && p1.value && p2.value) {
+      callback(date.value, p1.value, p2.value, p3.value, p4.value);
+      date.value = "";
+      p1.value = "";
+      p2.value = "";
+      p3.value = "";
+      p4.value = "";
+    }
+    else{
+      console.log('check input values: "'+
+                date.value+'" "'+
+                p1.value+'" "'+
+                p2.value+'"');
+    }
+  }
 
   setSubmitAction(submitCallback){
     // Add an event listener for the form submission
-    this.suggestionForm.addEventListener('submit', (event) => {
-      // Prevent the default behavior of the form submission
-      event.preventDefault();
-
-      // Get the input element
-      let date = document.getElementsByClassName('date-input')[0].value;
-      let p1 = document.getElementsByClassName('p1-input')[0].value;
-      let p2 = document.getElementsByClassName('p2-input')[0].value;
-      let p3 = document.getElementsByClassName('p3-input')[0].value;
-      let p4 = document.getElementsByClassName('p4-input')[0].value;
-
-      // Check if the input value is not empty
-      if (date && p1 && p2) {
-        submitCallback(date, p1, p2, p3, p4);
-      }
-      else{
-        console.log('check input values: "'+date+'" "'+p1+'" "'+p2+'"');
-      }
-    });
+    this.suggestionForm.removeEventListener('submit', this.#basicSubmitAction(this.#previousCallBack));
+    this.suggestionForm.addEventListener('submit', this.#basicSubmitAction(submitCallback));
+    this.#previousCallBack = submitCallback;
   }
 
   populateTable(matchData){
@@ -100,35 +140,7 @@ class ElementsController{
 }
 
 function exp10(x){
-    return Math.exp(Math.log(10) * x);
-}
-
-function get_win_probability(rating_a, rating_b){
-    let q_a = exp10(rating_a/400);
-    let q_b = exp10(rating_b/400);
-    let expected_a = q_a/(q_a+ q_b);
-    let expected_b = 1 - expected_a;
-    return ([expected_a, expected_b]);
-}
-
-function get_win_probabilities(elo_array){
-    let number_of_players = elo_array.length;
-    let win_prob_array = manual_copy(g_win_prob_template);
-    for(let ii=0; ii<number_of_players; ii++){
-        for(let jj=ii-1; jj>=0; jj--){
-            let win_prob = get_win_probability(elo_array[jj],elo_array[ii]);
-            win_prob_array[jj][ii-1] = win_prob[0];
-            win_prob_array[ii][jj] = win_prob[1];
-        }
-    }
-    return win_prob_array;
-}
-
-function get_elo(player){
-    if(g_elo_lookup[player] == null ){ // may be null, undefined
-        g_elo_lookup[player] = 1500; // create the new player
-    }
-    return g_elo_lookup[player];
+  return Math.exp(Math.log(10) * x);
 }
 
 let elo = new Elo();
