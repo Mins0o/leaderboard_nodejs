520f9ccf4cee498782ad7a154b0cc91d04c80286
Minsoo Kang <memesoo.kang@gmail.com>
2024-03-12 21:32:06 +0900

    adding chart element (test)

--- public/source.js
+++ public/source.js
@@ -248,6 +248,29 @@ serverComm.getDataFromServer().then(response => {
 });
 controller.setSubmitAction(serverComm.sendSuggestion);
 
+const temporaryChart = (()=>{
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+})()
+
 
 
 })();
